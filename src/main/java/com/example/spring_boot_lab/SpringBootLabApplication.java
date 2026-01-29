dd39f55a10391263b773ef5b93225ee1ec1e8ec3
tim12367 <tim12367@ymail.com>
2026-01-29 11:52:36 +0800

    init

---
+++ src/main/java/com/example/spring_boot_lab/SpringBootLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLabApplication.class, args);
+	}
+
+}
